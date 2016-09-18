d201983df2f13fc150a540a2cb850645b14e1a06
Jordan Bramble <jordanbramble@gmail.com>
2016-09-17 20:56:18 -0400

    set up for pip

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
-setup(name='simulacram',
-      version='0.1',
-      description='Create Pandas DataFrames of fake data with columns following distributions or other datatypes',
-      url='https://github.com/jbrambleDC/simulacram',
-      author='Jordan Bramble',
-      author_email='jordanbramble@gmail.com',
-      license='MIT',
-      packages=['simulacram'],
-      zip_safe=False)
+setup(name = 'simulacram',
+      version = '0.1',
+      description = 'Create Pandas DataFrames of simulated data with columns following statistical distributions or categorical datatypes',
+      url = 'https://github.com/jbrambleDC/simulacram',
+      author = 'Jordan Bramble',
+      author_email = 'jordanbramble@gmail.com',
+      license = 'MIT',
+      packages = ['simulacram'],
+      keywords = ['simulation', 'data', 'data science'],
+      zip_safe = False)
